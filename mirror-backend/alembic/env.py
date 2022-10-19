7a43396ad15a31d913db76b2c06aa2b7fdc4f72b
danwaldie <53157529+danwaldie@users.noreply.github.com>
2022-10-19 23:04:41 +0200

    alembic updates

--- mirror-backend/alembic/env.py
+++ mirror-backend/alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from ..config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -24,6 +26,8 @@ target_metadata = None
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option('sqlalchemy.url', settings.database_url)
 
 
 def run_migrations_offline() -> None:
