d933a44718ff7b942e922c352fda49aa2f10492d
danwaldie <53157529+danwaldie@users.noreply.github.com>
2022-10-22 12:58:19 +0200

    remove r string

--- mirror-backend/main.py
+++ mirror-backend/main.py
@@ -33,8 +33,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origin_regex=r'.*',
+    allow_origins=origins,
+    allow_origin_regex='.*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
