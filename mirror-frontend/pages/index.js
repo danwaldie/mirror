3237bc0c566f782cd7a543d74d0c779b08ea106f
danwaldie <53157529+danwaldie@users.noreply.github.com>
2022-10-24 22:26:31 +0200

    Thumbsy to Angry mood

--- mirror-frontend/pages/index.js
+++ mirror-frontend/pages/index.js
@@ -6,7 +6,7 @@ import {
     FaceFrownIcon,
     FaceSmileIcon as FaceSmileIconMini,
     FireIcon,
-    HandThumbUpIcon,
+    HandThumbDownIcon,
     HeartIcon,
     XMarkIcon,
 } from '@heroicons/react/20/solid'
@@ -19,7 +19,7 @@ const moods = [
     { name: 'Loved', value: 'loved', icon: HeartIcon, iconColor: 'text-white', bgColor: 'bg-pink-400' },
     { name: 'Happy', value: 'happy', icon: FaceSmileIconMini, iconColor: 'text-white', bgColor: 'bg-green-400' },
     { name: 'Sad', value: 'sad', icon: FaceFrownIcon, iconColor: 'text-white', bgColor: 'bg-yellow-400' },
-    { name: 'Thumbsy', value: 'thumbsy', icon: HandThumbUpIcon, iconColor: 'text-white', bgColor: 'bg-blue-500' },
+    { name: 'Angry', value: 'angry', icon: HandThumbDownIcon, iconColor: 'text-white', bgColor: 'bg-orange-500' },
     { name: 'I feel nothing', value: null, icon: XMarkIcon, iconColor: 'text-gray-400', bgColor: 'bg-transparent' },
 ]
 
